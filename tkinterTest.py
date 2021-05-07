42bb63d4a50ef6300df7e0e58bcf126620fca094
reaganroumph <79872816+reaganroumph@users.noreply.github.com>
2021-05-07 14:20:47 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -15,24 +15,48 @@ def exportPlaylist():
         for song in playlist:
             f.write("%s\n" % song)
 
-#this is a label widget
-L1 = Label(top, text="Playlist Generator")
-L1.grid(column= 0, row= 1)
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
 
-#this is a text entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
+def mainMenu():
+    clearWindow()
+    Lmain = Label(top, text = "Block 5 GUIs")
+    Lmain.grid(column = 0, row = 1)
+    
+    B1Main = Button(text = "week 1", bg = "green", command = week1)
+    B1Main.grid(column = 0, row = 2)
 
-#this is a button widget
-B1 = Button(text= " + ", bg = "#35783d", command = addSong)
-B1.grid(column = 1, row = 2)
+    B2Main = Button(text = "week 2", bg = "green")
+    B2Main.grid(column = 0, row = 3)
+    
+    B3Main = Button(text = "week 3", bg = "green")
+    B3Main.grid(column = 0, row = 4)
 
-B2 = Button(text = "Print List", bg = "#35783d", command = printList)
-B2.grid(column=0, row = 3)
+def week1():
+    clearWindow()
+
+    #this is a label widget
+    L1 = Label(top, text="Playlist Generator")
+    L1.grid(column= 0, row= 1)
+
+    #this is a text entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column = 0, row = 2)
+
+    #this is a button widget
+    B1 = Button(text= " + ", bg = "#35783d", command = addSong)
+    B1.grid(column = 1, row = 2)
+
+    #print
+    B2 = Button(text = "Print List", bg = "#35783d", command = printList)
+    B2.grid(column=0, row = 3)
+
+    #export
+    B3 = Button(text= "Export List", bg = "#35783d", command = exportPlaylist)
+    B3.grid(column=1, row = 3)
 
 
-B3 = Button(text= "Export List", bg = "#35783d", command = exportPlaylist)
-B3.grid(column=1, row = 3)
-
-
-top.mainloop()
+if __name__== "__main__":
+    mainMenu()
+    top.mainloop()
